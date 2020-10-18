ebd8cae8c2386aac0e254bd2fb456a6b78fa1c49
ethan <ethan@LAPTOP-FHOLCKVQ>
2020-10-18 12:56:30 -0700

    18/10 (2)

--- src/_05_body_part_quiz/BodyPartQuiz.java
+++ src/_05_body_part_quiz/BodyPartQuiz.java
@@ -51,24 +51,76 @@ public class BodyPartQuiz {
 	private void startQuiz() {
 
 		// 1. Make an int variable to hold the score.
+		int score = 0;
 
-		// 2. Set the size of the window in the initializeGui() method 
+		// 2. Set the size of the window in the initializeGui() method
+		initializeGui();
 
 		// 4. Ask the user who this person is and store their answer
 		String guess = JOptionPane.showInputDialog("who is this?");
 
 		// 5. Check their answer. If they guessed correctly:
 		// -- Tell them they are right and increase the score by 1
+		if (guess.equalsIgnoreCase("Arnold Schwarzenegger")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		}
 
 		// 6. Otherwise:
 		// -- Tell them they are wrong and who the person is
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+			score -= 1;
+		}
 
 		// 7. Use the showNextImage() method below to get the next image
 		showNextImage();
 
 		// 8. .... repeat 4-7 for all your images.....
 
+		String guess2 = JOptionPane.showInputDialog("who is this?");
+
+		if (guess2.equalsIgnoreCase("Leonardo DiCaprio")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+			score -= 1;
+		}
+
+		showNextImage();
+
+		String guess3 = JOptionPane.showInputDialog("who is this?");
+
+		if (guess3.equalsIgnoreCase("Morgan Freeman")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+			score -= 1;
+		}
+
+		showNextImage();
+		
+		String guess4 = JOptionPane.showInputDialog("who is this?");
+
+		if (guess4.equalsIgnoreCase("Jack Black")) {
+			JOptionPane.showMessageDialog(null, "Correct!");
+			score += 1;
+		}
+
+		else {
+			JOptionPane.showMessageDialog(null, "Incorrect!");
+			score -= 1;
+		}
+
 		// 9. Show them their current score
+		
+		JOptionPane.showMessageDialog(null, "Your score is " + score);
 
 	}
 
